0f897fa2b854fbf8181541ad54ed76f0574cdc5b
MasterEda92 <50300987+MasterEda92@users.noreply.github.com>
2019-05-16 22:35:00 +0200

    translate to english and added variable display time

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@
 
 const { ipcRenderer } = require('electron')
 
-// Globals
-let FlipCardCounter = 0; // Counter der für die Erzeugung der IDs benutzt wird
-let arrAufgedeckteIDs = []; // Array für Aufgedeckte Image-IDs
-let anzImages = 0; // Anzahl der ausgewählten Bilder (dupliziert)
-// Variablen für ausgewählte/geklickte Images
+// globals
+let flipCardCounter = 0; // counter for flip cards
+let arrRevealedIDs = []; // array for revealed Image-IDs
+let countImages = 0; // count of selected images (duplicated)
+
+// variables for clicked images
 let IdFirstSelected = undefined;
 let IdSecondSelected = undefined; 
 
-// Sleep-Funktion
+// set display time default
+$('#display-time').val(2000);
+
+// sleep-function
 const sleep = (milliseconds) => 
 {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-// FlipCard-HTML erstellen
+// create FlipCard-HTML
 function createFlipCardHtml (value)
 {
-    ++FlipCardCounter;
-    let cardId = `card${FlipCardCounter}`;
+    ++flipCardCounter;
+    let cardId = `card${flipCardCounter}`;
 
-    // HTML-Baustein erzeugen
+    // create HTML block
     var newElem = $(
     `<div class="mix col-sm-3 my-4 py-3">
         <div class="flip_card" id="${cardId}" my-4>
@@ -35,76 +39,75 @@ function createFlipCardHtml (value)
         </div>
     </div>`);
 
-    // HTML-Baustein einfügen
-    $('#bilder_container').append(newElem);
+    // insert HTML block
+    $('#images_container').append(newElem);
 }
 
-// FlipCards erstellen und alles was dazu gehört
+// create flip cards from images array
 function createFlipCards (Images)
 {
-    // Alle bestehenden FlipCards (Map ebenfalls) löschen und Counter zurücksetzen
-    $('#bilder_container').children().remove();
-    arrAufgedeckteIDs.length = 0;
-    FlipCardCounter = 0;
+    // delete flip cards and reset counter
+    $('#images_container').children().remove();
+    arrRevealedIDs.length = 0;
+    flipCardCounter = 0;
 
-    anzImages = Images.length;
+    countImages = Images.length;
 
-    // neue FlipCards anlegen
+    // create new flip cards
     Images.forEach (createFlipCardHtml);
 
-    // Aktionen neu erstellen
+    // create actions
     $(".flip_card").flip(
     {
         trigger: 'manual'
     });
 
-    // Click auf FlipCard behandeln 
+    // flip card click event
     $('.flip_card').click((e) => 
     {
         let ID = $('#' + e.target.id).parentsUntil('.flip_card').parent().attr('id');
         let ImgUrl = $(`#img_b${ID}`).attr('src');
 
-        // Click auf aufgedecktes/aufgelöstes Image abfangen
-        if (ID === IdFirstSelected || arrAufgedeckteIDs.includes (ID))
+        // catch click on revealed image
+        if (ID === IdFirstSelected || arrRevealedIDs.includes (ID))
         {
             return;
         }
 
-        if (IdFirstSelected === undefined) // erstes Image auswählen
+        if (IdFirstSelected === undefined) // first image selected
         {
             IdFirstSelected = ID;
             $('#' + ID).flip('toggle');    
         }
-        else if (IdSecondSelected === undefined) // zweites Image auswählen
+        else if (IdSecondSelected === undefined) // secend image selected
         {
             $('#' + ID).flip('toggle');
 
-            // 1 Sekunde warten bevor weiter gemacht wird
-            sleep(1000).then(() => 
+            sleep($('#display-time').val()).then(() => 
             { 
                 IdSecondSelected = ID;
                 let ImgUrlFirst = $(`#img_b${IdFirstSelected}`).attr('src');
                 if (ImgUrlFirst === ImgUrl)
                 {
-                    // Paar gefunden
-                    // IDs merken
-                    arrAufgedeckteIDs.push(IdFirstSelected);
-                    arrAufgedeckteIDs.push(IdSecondSelected);
-                    // Variablen zurücksetzen und nicht zurücktogglen
+                    // pair found
+                    // remember IDs
+                    arrRevealedIDs.push(IdFirstSelected);
+                    arrRevealedIDs.push(IdSecondSelected);
+                    // reset variables
                     IdFirstSelected = undefined;
                     IdSecondSelected = undefined;
 
-                    // alle gelöst?
-                    if (arrAufgedeckteIDs.length === anzImages)
+                    // all images revealed?
+                    if (arrRevealedIDs.length === countImages)
                     {
-                        alert ('Glückwunsch! Sie haben das Spiel gelöst!\nUm ein neues Spiel zu starten klicken Sie einfach auf "Bilder auswählen".');
+                        alert ('Gratulations! You solved the game!\nTo restart the game click the button "select images".');
                     }
                 }
                 else
                 {
-                    // Paar nicht gefunden
+                    // pair not found
                     
-                    // Bilder zurücktogglen, Variablen rücksetzen und IDs merken
+                    // toggle image, reset variables and remember IDs
                     $('#' + IdFirstSelected).flip('toggle');
                     $('#' + IdSecondSelected).flip('toggle');
                     IdFirstSelected = undefined;
@@ -112,33 +115,33 @@ function createFlipCards (Images)
                 }
             })
         }
-        else // Irgendwas ist schief gelaufen!
+        else // fatal error
         {
-            alert('Fehler aufgetreten! Das Programm wird beendet!');
+            alert('Fatal error! The app will be terminated!');
             ipcRenderer.send('quit-app');
         }
     })
 }
 
-// Funktionen und Handler initialisieren
+// initialize functions and handlers
 $(document).on('ready', function() 
 {
-    // bei Programmstart Bilder abfragen
+    // get images on start
     ipcRenderer.send('get-images');
 
-    // beim Button-Click Bilder abfragen
+    // get images on click
     $('#btn-select-images').on('click', () => 
     {
         ipcRenderer.send('get-images');
     });
 
-    // Bilder vom Hauptprozess übernehmen
+    // get images from main process
     ipcRenderer.on('get-images-reply', (event, arg) => 
     {
         createFlipCards(arg);
     });
 
-    // App beenden
+    // quit app
     $('#btn-quit').on('click', () => 
     {
         ipcRenderer.send('quit-app');
